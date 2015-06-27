cef6745707c35f8d11438665cd217ccb1127fda2
Felan Carlo Garcia <felancarlogarcia@gmail.com>
2015-06-27 22:04:05 +0800

    Create cost-function.py
    simple cost function implementation, based on the Machine Learning Class of Andrew Ng

---
+++ gradient-descent/cost-function.py
@@ -0,0 +1,28 @@
+#
+# Simple cost function implementation.
+# Based on Andrew Ng's Standford 
+# Machine Learning Class
+#
+
+
+# theta0 and theta1 are usually randomized.
+# For this application, we set theta0 and
+# theta2 manually in order to answer the
+# question for the Week1 of the Machine
+# learning class.
+def cost_function(x,y,theta0,theta1): 
+    m = len(x)  
+    return sum([(theta0 + theta1*x[i] - y[i])**2 for i in range(m)]) / (2.0 * m)
+
+
+
+def main():
+    x  = [3,1,0,4]
+    y  = [2,2,1,3]
+    t0 = 0
+    t1 = 1
+    print cost_function(x,y,t0,t1)
+
+
+if __name__ == '__main__':
+	main()
