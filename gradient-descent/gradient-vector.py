31545f2511f61273b277ab5ce3477666d2a47b36
Felan Carlo Garcia <felancarlogarcia@gmail.com>
2015-07-05 19:20:05 +0800

    Update gradient-vector.py

--- gradient-descent/gradient-vector.py
+++ gradient-descent/gradient-vector.py
@@ -14,7 +14,7 @@ def grad_desc_vector(X,y, alpha, max_iter):
 # We take advantage of the numpy matrix operations
 # to speed up the gradient descent computation
     m       = y.shape[0]
-    theta   = np.ones(X.shape[1])
+    theta   = np.zeros(X.shape[1])
     X_trans = X.T 
 
     for loop in range(max_iter):
@@ -31,13 +31,11 @@ def main():
     
     # load the dataset to the two variables
     X, y = make_regression(n_samples=100, n_features=1, n_informative=1, random_state=0, noise=35) 
-    
     m = np.shape(X)[0]
-  
     X = np.c_[ np.ones(m), X]
 
     # get the slope
-    theta = grad_desc_vector(X, y, 0.01,  1000)
+    theta = grad_desc_vector(X, y, 0.001, 1500)
 
     print theta   
     
